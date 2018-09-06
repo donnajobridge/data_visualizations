7d2b562a0c97b2c1f572a030cad0455c09ca6c61
donnajobridge <donnajb@gmail.com>
2018-09-06 12:53:55 -0500

    added readme for rencode

---
+++ rencode/README.md
@@ -0,0 +1,18 @@
+*Project information:*
+Experiment 1 from Active Learning (http://www.donnajobridge.com/active.html)
+
+*What is created:*
+A confusion matrix is made for Active and Passive recognition performance (hits and false alarms).
+
+Swarm plots and box plots are created for the following measures:
+
+Recall
+Precision
+F1
+
+Hit rate
+False alarm rate
+*d* prime
+
+*How to run:*
+To generate figures from behavioral data, go to terminal in the rencode directory. From there, run "python run_rencode.py"
--- rencode/rencode_figs.py
+++ rencode/rencode_figs.py
@@ -9,31 +9,32 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
 def make_rencode_figs(dprimedf, measure_array, conf):
-    # for fig_type, myplot in [('bar', sns.barplot), ('swarm', sns.swarmplot),
-    # ('box', sns.boxplot)]:
     for fig_type, myplot in [('swarm', sns.swarmplot), ('box', sns.boxplot)]:
         for measure in measure_array:
+            measure_cap = measure.capitalize()
             ax=myplot(x='cond', y=measure,
             data=dprimedf, palette="colorblind")
             ax.set_xlabel('Condition', fontsize=20)
+            ax.set_xticklabels({'Active':0, 'Passive':1})
             ax.tick_params(labelsize=16)
             plt.legend(fontsize=12)
             plt.gca().legend().set_title('')
-            ax.set_ylabel(measure, fontsize=20)
-            plt.title('Recognition Performance', fontsize=30)
+            ax.set_ylabel(measure_cap + ' Score', fontsize=20)
+            plt.title(measure_cap, fontsize=30)
             fig=ax.get_figure()
             fig.savefig('figs/rencode_'+fig_type+measure+conf+'.png')
             plt.clf()
 
 def make_cm_matrix(rencode):
     for cond, ldf in rencode.groupby('cond'):
+        cond_cap = cond.capitalize()
         trialtype = ldf['old/new']
         resp = ldf['recog_answer']
         cm=confusion_matrix(trialtype, resp)
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         cmfig = sns.heatmap(cm, cmap='Blues', annot=True, fmt='g', vmin=0, vmax=.8)
-        plt.title(cond)
+        plt.title(cond_cap, fontsize=30)
         cmfig = cmfig.get_figure()
-        fname = 'figs/rencode_cm_.png'
+        fname = 'figs/rencode_cm_' + cond + '.png'
         cmfig.savefig(fname)
         plt.clf()
